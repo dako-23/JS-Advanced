3f736b01dca1c234a110b22385ca1851841de9bd
dako-23 <iordan.megaparts@gmail.com>
2024-09-12 14:23:34 +0300

    .

---
+++ Syntax, Functions and Statements/01.Echo-Function.js
@@ -0,0 +1,15 @@
+function solve (text){
+
+    console.log(text.length);
+    console.log(text);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    solve('Hello world')
---
+++ Syntax, Functions and Statements/02.String-length.js
@@ -0,0 +1,14 @@
+function solve(text1, text2, text3) {
+
+    let totalLength = text1.length + text2.length + text3.length;
+    let avarageLength = Math.floor(totalLength / solve.length);
+
+    
+    console.log(totalLength);
+    console.log(avarageLength);
+
+
+
+
+}
+solve('java script', 'java brat', 'java sestraxDd');
