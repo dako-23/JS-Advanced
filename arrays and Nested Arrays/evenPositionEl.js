304dba3b35eeb4e011409c550f51fcec383776b6
dako-23 <iordan.megaparts@gmail.com>
2024-09-16 16:12:31 +0300

    .

--- arrays and Nested Arrays/evenPositionEl.js
+++ arrays and Nested Arrays/evenPositionEl.js
@@ -5,14 +5,21 @@ function solve(arr) {
     for (let i = 0; i < arr.length; i += 2) {
 
         result.push(arr[i]);
-        
-        
+
 
     }
 
-    console.log(result.join(' '));
+    // console.log(result.join(' '));
+
+
+    let [a, b, ...jack] = arr
+
+    console.log(jack);
+    
+    
+    
 
 
 }
 solve(['20', '30', '40', '50', '60']);
-solve(['5', '10'])
+// solve(['5', '10'])
