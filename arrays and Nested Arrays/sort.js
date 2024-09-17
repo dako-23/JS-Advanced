01cf579ee3928b3ed7faead3b67e76db4aebeec1
dako-23 <iordan.megaparts@gmail.com>
2024-09-17 14:58:14 +0300

    .

---
+++ arrays and Nested Arrays/sort.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+
+    return arr.sort((a, b) => a.localeCompare(b))
+
+
+
+
+
+}
+console.log(solve(['john', 'alice', 'nestor', 'Tomas']))
---
+++ arrays and Nested Arrays/sumFirstAndLast.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+
+    return Number(arr.shift()) + Number(arr.pop())
+
+
+
+
+
+
+
+
+
+
+}
+console.log(solve(['10', '15', '20']));
+
+
+
+
