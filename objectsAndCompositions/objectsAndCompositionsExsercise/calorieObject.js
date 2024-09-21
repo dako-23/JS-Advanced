a6615481338291e9d24b8f8c27ae7447d1fe8cf4
dako-23 <iordan.megaparts@gmail.com>
2024-09-21 23:44:05 +0300

    .

---
+++ objectsAndCompositions/objectsAndCompositionsExsercise/calorieObject.js
@@ -0,0 +1,22 @@
+function solve(arr) {
+
+    const result = {};
+    // let cal = [];
+
+    arr.forEach((el, i) => {
+
+        
+        
+
+    });
+
+    
+
+    console.log(result);
+
+
+
+
+
+}
+solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
