ae2f015c551a2b63e4277d64ee155624dfb4367c
dako-23 <iordan.megaparts@gmail.com>
2024-09-23 12:18:52 +0300

    .

--- objectsAndCompositions/cityRecord2.js
+++ objectsAndCompositions/cityRecord2.js
@@ -1,42 +1,49 @@
-function solve(name, population, treasury) {
+function factory(library, orders) {
+    return orders.map(order => {
 
-    const city = {
-        name,
-        population,
-        treasury,
-    };
+        const product = Object.assign({}, order.template);
 
-    return city;
 
+        order.parts.forEach(part => {
+            product[part] = library[part];
+        });
+
+        return product;
+    });
 }
 
-function addTaxBehavior(city) {
-    return Object.assign(city, {
-        taxRate: 10,
 
-        collectTaxes() {
-            this.treasury += Math.floor(this.population * this.taxRate)
-        },
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
 
-        applyGrowth(percent) {
-            this.population += Math.floor((percent / 100) * this.population)
-
-        },
-
-        applyRecesion(percent) {
-            this.treasury -= Math.ceil((percent / 100) * this.treasury)
-        }
-    })
-
-}
-const result = solve('Tortuga',
-    7000,
-    15000);
-
-console.log(result);
-console.log(JSON.stringify(result));
-
-// addTaxBehavior(result)
-// console.log(result);
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
 
 
+const products = factory(library, orders);
+console.log(products);
