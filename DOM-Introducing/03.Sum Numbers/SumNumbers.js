66f7d384139b6a2b2c380801fdc4f84e7942d297
dako-23 <iordan.megaparts@gmail.com>
2024-09-24 16:38:01 +0300

    .

--- DOM-Introducing/03.Sum Numbers/SumNumbers.js
+++ DOM-Introducing/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,19 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    
+    const a = document.getElementById('num1');
+    const b = document.getElementById('num2');
+    const output = document.getElementById('sum')
+
+    const sum = Number(a.value) + Number(b.value);
+    output.value = sum
+
+
+
+
+
+
+
+
+
+
 }
