8f5669a192bcfc29f6a2d38f6e9eec32ac36b8f2
dako-23 <iordan.megaparts@gmail.com>
2024-09-24 16:04:34 +0300

    ..

--- DOM-Introducing/02.Collect List Items/CollectListItems.js
+++ DOM-Introducing/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,17 @@
 function extractText() {
-    // TODO
+
+    const input = document.getElementById('items');
+    const output = document.getElementById('result');
+
+    const result = [];
+
+    for (const element of input.children) {
+        result.push(element.textContent);
+
+    }
+
+    output.textContent = result.join('\n')
+
+
+
 }
