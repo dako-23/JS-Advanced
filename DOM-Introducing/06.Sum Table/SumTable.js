cdd92fb268914038c2c1ffa760cf7d25b6e910be
dako23 <iordan.megaparts@gmail.com>
2024-09-26 18:07:34 +0300

    .

--- DOM-Introducing/06.Sum Table/SumTable.js
+++ DOM-Introducing/06.Sum Table/SumTable.js
@@ -1,3 +1,18 @@
 function sumTable() {
 
+
+    const tableRows = document.querySelectorAll('table tr')
+    const rows = Array.from(tableRows).slice(1, -1)
+    const cols = rows.map(r => r.children[r.children.length - 1]);
+    
+    let sum = 0
+
+    for (const col of cols) {
+
+        sum += Number(col.textContent);
+        
+    }
+
+    output.textContent = sum
+
 }
