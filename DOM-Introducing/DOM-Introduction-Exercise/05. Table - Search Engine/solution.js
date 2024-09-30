2892335ac88479357c9a70fdc637127daa2a1257
dako-23 <iordan.megaparts@gmail.com>
2024-09-30 16:39:14 +0300

    .

--- DOM-Introducing/DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
+++ DOM-Introducing/DOM-Introduction-Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,36 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+
+      const studentsListRows = document.querySelectorAll('table tbody tr');
+      const searchFieldEl = document.querySelector('#searchField')
+      const searchStrEl = searchFieldEl.value.toLowerCase();
+
+      if(searchStrEl == '') return
+
+      const studentList = Array.from(studentsListRows).map((el) => el.innerText.toLowerCase());
+
+      const result = [];
+
+      studentsListRows.forEach((el) => {
+         el.classList.remove('select');
+      })
+
+      for (let i = 0; i < studentList.length; i++) {
+         if (studentList[i].indexOf(searchStrEl) >= 0) {
+            result.push(i);
+         }
+      }
+
+      result.forEach((pos, index) => {
+         studentsListRows[pos].classList.add('select');
+      })
+
+
+      searchFieldEl.value = ''
+
+
+
 
    }
 }
