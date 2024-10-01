90103d97ad8de8f7fcd051e7e4a9792a50a18373
dako-23 <iordan.megaparts@gmail.com>
2024-10-01 14:16:16 +0300

    .

--- DOM-Introducing/DOM-Introduction-Exercise/08. Generate Report/report.js
+++ DOM-Introducing/DOM-Introduction-Exercise/08. Generate Report/report.js
@@ -1,3 +1,35 @@
 function generateReport() {
-    //TODO
+
+    const outputEl = document.querySelector('#output');
+    const tHeadRow = document.querySelector('table thead tr')
+    const tBodyRows = document.querySelectorAll('table tbody tr ')
+
+    const checkInputElements = [...tHeadRow.children]
+        .map((item, index) => ({
+            el: item.children[0],
+            name: item.children[0].getAttribute('name'),
+            index: index
+        })).filter((item) => item.el.checked);
+
+
+    const outputData = [...tBodyRows]
+        .map((tr) => {
+            return checkInputElements.reduce((acc, curr) =>{
+                acc[curr.name] = tr.children[curr.index].textContent;
+                return acc
+            }, {})
+        });
+        
+        outputEl.value = JSON.stringify(outputData);
+
+
+
+
+
+
+
+
+
+
+
 }
