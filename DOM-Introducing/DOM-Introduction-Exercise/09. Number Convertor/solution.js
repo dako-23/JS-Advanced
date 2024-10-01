c414ab633f8cf3ba80c85626f5ba3aba89c504bd
dako-23 <iordan.megaparts@gmail.com>
2024-10-01 15:36:55 +0300

    .

--- DOM-Introducing/DOM-Introduction-Exercise/09. Number Convertor/solution.js
+++ DOM-Introducing/DOM-Introduction-Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,34 @@
 function solve() {
 
-    //TODO...
+    const selectConvertorTo = document.querySelector('#selectMenuTo');
+
+    const optionalSelectElConvToHex = document.createElement('option');
+    optionalSelectElConvToHex.setAttribute('value', 'hexadecimal');
+    optionalSelectElConvToHex.textContent = 'Hexadecimal'
+
+    const optionalSelectElConvToBinary = document.createElement('option');
+    optionalSelectElConvToBinary.setAttribute('value', 'binary');
+    optionalSelectElConvToBinary.textContent = 'Binary'
+
+    selectConvertorTo.appendChild(optionalSelectElConvToHex);
+    selectConvertorTo.appendChild(optionalSelectElConvToBinary);
+
+    const resultEl = document.querySelector('#result')
+
+    document.querySelector('#container button').addEventListener('click', (e) => {
+        const input = Number(document.querySelector('#input').value)
+
+        if (selectConvertorTo.value == 'binary') {
+            resultEl.value = input.toString(2)
+        } else if (selectConvertorTo.value == 'hexadecimal') {
+            resultEl.value = input.toString(16).toUpperCase()
+        }
+    })
+
+
+
+
+
+
+
 }
