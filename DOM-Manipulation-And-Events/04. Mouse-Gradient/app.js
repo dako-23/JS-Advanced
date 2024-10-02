1d69edf1bdfe52b4c0b9d4aa3781f524ea10773e
dako-23 <iordan.megaparts@gmail.com>
2024-10-02 17:49:08 +0300

    .

--- DOM-Manipulation-And-Events/04. Mouse-Gradient/app.js
+++ DOM-Manipulation-And-Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    const boxEl = document.querySelector('#gradient')
+    const output = document.querySelector('#result')
+    boxEl.addEventListener('mousemove', onHover);
+
+    function onHover(event) {
+
+        const offset = Math.floor(event.offsetX / boxEl.clientWidth * 100) + '%';
+
+        output.textContent = offset
+    }
+
+
+
+
 }
