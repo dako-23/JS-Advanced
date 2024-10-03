0d8a037dbde585aca6b4be1e0c407c173f4fd725
dako-23 <iordan.megaparts@gmail.com>
2024-10-03 16:29:08 +0300

    .

--- DOM-Introducing/DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
+++ DOM-Introducing/DOM-Introduction-Exercise/07. Hell's Kitchen/app.js
@@ -2,9 +2,21 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick() {
-      const inputEl = document.querySelector('#inputs')
+      const inputEl = document.querySelector('#inputs');
+      const bestRestaurantEl = document.querySelector('#bestRestaurant')
 
-      
+      const newItem = document.createElement('li')
+      const text = inputEl.value
+
+      if (!text) return;
+
+      bestRestaurantEl.textContent = text;
+
+      listEl.appendChild(newItem)
+
+      inputEl.value = ''
+
+
 
 
    }
