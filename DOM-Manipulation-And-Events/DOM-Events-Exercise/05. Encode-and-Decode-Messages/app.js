2f0ee5854954e352fd4499556ac8fa4c71307ffb
dako-23 <iordan.megaparts@gmail.com>
2024-10-04 17:53:57 +0300

    .

--- DOM-Manipulation-And-Events/DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM-Manipulation-And-Events/DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,37 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    document.querySelector('#main > div:nth-child(1) button').addEventListener('click', (e) => {
+
+        const encodeInputEl = e.target.parentNode.querySelector('textarea');
+        const message = encodeInputEl.value;
+        const receiverEl = document.querySelector('#main > div:nth-child(2) button')
+
+        const encodedMessage = message.split('').map((ch) => String.fromCharCode(ch.charCodeAt() + 1)).join('');
+
+
+        receiverEl.value = encodedMessage;
+        encodeInputEl.value = '';
+
+    });
+
+    document.querySelector('#main > div:nth-child(2) button').addEventListener('click', (e) => {
+
+        const decodeInputEl = e.target.parentNode.querySelector('textarea');
+        const message = decodeInputEl.value;
+
+        const decodedMessage = message.split('').map((ch) => String.fromCharCode(ch.charCodeAt() - 1)).join('');
+
+        decodeInputEl.value = decodedMessage
+
+
+    });
+
+
+
+
+
+
+
+
+
 }
