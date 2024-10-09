5672e1caef58f3e5423ec1fed556e967fc5ddf3e
dako-23 <iordan.megaparts@gmail.com>
2024-10-09 13:23:31 +0300

    .

---
+++ unit-Test-and-Error-Handling/app.js
@@ -0,0 +1,13 @@
+function addNumbers(a, b) {
+
+    if (typeof a != 'number' || typeof b != 'number') {
+        throw new TypeError('Must provide numbers')
+    };
+
+    return a + b;
+
+}
+
+export {
+    addNumbers
+}
---
+++ unit-Test-and-Error-Handling/app.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+
+import { addNumbers } from './app.js';
+
+describe('Adding numbers', function () {
+    it('works with numbers', () => {
+        expect(addNumbers(1, 1)).to.equal(2);
+    });
+
+    it('doesnt work', () => {
+        expect(() => addNumbers('a', 'b').to.throw());
+
+    });
+})
