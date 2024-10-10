909dae6f36410d072fdbb85e5b94c375af9d8b2d
dako-23 <iordan.megaparts@gmail.com>
2024-10-10 15:14:45 +0300

    .

--- evenOrOdd.test.js
+++ evenOrOdd.test.js
@@ -1,8 +1,33 @@
 import { expect } from 'chai';
 import { isOddOrEven } from '../evenOrOdd.js';
+import { describe, it } from 'mocha';
 
-describe('test', function () {
-    it('Test', () => {
-        expect(true).to.be.true;
+describe('Check undefined', function () {
+    it('Number', () => {
+        expect(isOddOrEven(1)).to.be.undefined;
+    })
+    it('Array', () => {
+        expect(isOddOrEven(['string'])).to.be.undefined
+    })
+    it('Object', () => {
+        expect(isOddOrEven({ name: 'george' })).to.be.undefined
+    })
+
+    describe('Happy path Even', function () {
+        it('Is even', () => {
+            expect(isOddOrEven('11')).to.equal('even')
+        })
+        it('Is even', () => {
+            expect(isOddOrEven('test')).to.equal('even')
+        })
+    })
+
+    describe('Happy path Odd', function () {
+        it('Is odd', () => {
+            expect(isOddOrEven('1')).to.equal('odd')
+        })
+        it('Is odd', () => {
+            expect(isOddOrEven('tes')).to.equal('odd')
+        })
     })
 })
