207bd744dbb4986ee9565f8c52ae6eceda1ad8d8
dako-23 <iordan.megaparts@gmail.com>
2024-10-14 13:56:28 +0300

    .

---
+++ Classes-and-Attributes-Lab-Exercise/03.tickets.js
@@ -0,0 +1,48 @@
+function solve(input, criteria) {
+
+    let tickets = [];
+
+
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination
+            this.price = price
+            this.status = status
+        }
+
+        static sort(tickets, criteria) {
+
+            if (criteria === 'price') {
+                tickets.sort((a, b) => a[criteria] - b[criteria])
+            } else {
+                tickets.sort((a, b) => a[criteria].localeCompare(b[criteria]));
+            }
+        }
+    }
+
+    for (const line of input) {
+
+        [destination, price, curStatus] = line.split('|')
+
+        const ticket = new Ticket(destination, Number(price), curStatus);
+        console.log(ticket);
+        
+        
+
+        tickets.push(ticket);
+
+
+
+    }
+
+    Ticket.sort(tickets, criteria);
+    return tickets
+
+}
+console.log(solve(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'));
+
+
